036882369f2676eca453802440063131e9f71c2d
kasra <kasra@3C-F8-62-34-0C-88.kasra.com>
2025-01-03 05:37:30 -0500

    fixed master card 10 bug

--- src/components/cards/masters/MasterCard10.jsx
+++ src/components/cards/masters/MasterCard10.jsx
@@ -9,14 +9,14 @@ function MasterCard10() {
           <img
               src={Godarzi}
               alt="Godarzi"
-              className="w-full h-60 sm:h-72 lg:h-80 shadow-lg rounded-t-lg"
+              className="w-48 h-48 sm:w-96 sm:h-96 shadow-lg rounded-t-lg"
           />
           {/* نام */}
-          <div className="bg-secondary w-full text-center text-lg sm:text-xl font-medium py-2">
+          <div className="bg-secondary w-48 sm:w-96 text-center text-lg sm:text-xl font-medium py-2">
              خانم گودرزی
           </div>
           {/* رشته ورزشی */}
-          <div className="bg-secondary w-full text-center text-sm sm:text-base font-medium py-2 rounded-b-lg">
+          <div className="bg-secondary w-48 sm:w-96 text-center text-sm sm:text-base font-medium py-2 rounded-b-lg">
             نینجوتسو
           </div>
         </div>
